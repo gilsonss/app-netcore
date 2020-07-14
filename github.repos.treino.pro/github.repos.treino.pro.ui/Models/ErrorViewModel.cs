68be52995cba5428b2a88a8a40c8f78b383e0de9
Gilson Silva <gilson.ss07@gmail.com>
2020-07-13 22:13:18 -0300

    versao : projeto inicial

---
+++ github.repos.treino.pro/github.repos.treino.pro.ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace github.repos.treino.pro.ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
